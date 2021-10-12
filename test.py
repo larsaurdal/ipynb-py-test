b81f3d18d29a5f47cd6f33921fc582f372b837c2
Lars Aurdal <lars@findable.no>
2021-10-12 11:53:04 +0200

    Initial version

---
+++ test.py
@@ -0,0 +1,21 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# Imports
+
+# %%
+import numpy as np
+
+# %% [markdown]
+# Do some dummy calculation
+
+# %%
+a = np.array([[1, 2,3], [4, 5, 6], [7, 8, 9]])
+
+
+# %%
+a
+
+# %% [markdown]
+# 
+
