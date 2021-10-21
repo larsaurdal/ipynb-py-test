5ae59b58f3aef9bb293156790ff7bd12c115451f
Lars Aurdal <lars@findable.no>
2021-10-21 13:12:26 +0200

    Initial version

---
+++ test2.py
@@ -0,0 +1,44 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: 'Python 3.9.6 64-bit (''Findable'': pyenv)'
+#     name: python3
+# ---
+
+# %% [markdown]
+# ### Imports
+
+# %%
+import numpy as np
+
+# %% [markdown]
+# ### Calculations
+
+# %%
+a = np.random.rand()
+
+# %%
+b = np.ceil(a)
+
+# %%
+a
+
+# %%
+b
+
+# %% [markdown]
+### Do some more tests
+
+# %%
+d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# %%
+d
+# %%
